cdd88c065233d76d9c7e2c2cfb57682ebe51003a
Chompy <me@chompy.co>
2014-02-17 19:15:42 -0500

    added tags extension and gallery extension (extension for tags)

---
+++ extension/gallery.py
@@ -0,0 +1,95 @@
+"""
+    WebChomp -- A tool for generating static websites.
+    Copyright (C) 2014 Nathan Ogden
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import urlparse
+
+"""
+    Tag extension, added a gallery tag that supports images and youtube videos
+"""
+class tag_extension:
+
+    def __init__(self, generator):
+        self.generator = generator
+
+    def get_tags(self):
+        return {
+            'gallery' : self.gallery_tag
+        }
+
+    # get youtube id from url
+    def _youtube_id(self, value):
+        query = urlparse.urlparse(value)
+        if query.hostname == 'youtu.be':
+            return query.path[1:]
+        if query.hostname in ('www.youtube.com', 'youtube.com'):
+            if query.path == '/watch':
+                p = urlparse.parse_qs(query.query)
+                return p['v'][0]
+            if query.path[:7] == '/embed/':
+                return query.path.split('/')[2]
+            if query.path[:3] == '/v/':
+                return query.path.split('/')[2]
+        # fail?
+        return None
+
+    """ Creates a gallery from definition in current page info. """
+    def gallery_tag(self, gallery):
+
+        # get page info
+        page_info = self.generator.current_page_info
+
+        # make sure tag properties exist
+        if not "_tags" in page_info: return "[TAG: GALLERY (Tag attributes not found.)]"
+        if not "gallery" in page_info['_tags']: return "[TAG: GALLERY (Tag attributes not found.)]"
+        if not gallery in page_info['_tags']['gallery']: return "[TAG: GALLERY (Tag attributes not found.)]"
+
+        # generate gallery html
+        gallery_html = ""
+        for item in page_info['_tags']['gallery'][gallery]:
+            
+            # make sure url provided
+            if not 'url' in item: continue
+
+            # get title
+            title = item['title'] if 'title' in item else ""
+
+            # get url
+            url = item['url']
+
+            # process asset
+            image_src = ""
+
+            # youtube link
+            if item['type'].lower() == "youtube":
+                youtube_id = self._youtube_id(url)
+                image_src = "http://img.youtube.com/vi/%s/0.jpg" % youtube_id
+
+            # assume image asset if nothing else specified
+            else:
+                item['type'] = "image"
+                if "asset" in self.generator.jinja_functions and "add" in self.generator.jinja_functions['asset']:
+                    url = self.generator.jinja_functions['asset']['add'](url)
+                if "asset" in self.generator.jinja_functions and "image" in self.generator.jinja_functions['asset']:                    
+                    image_src = self.generator.jinja_functions['asset']['image'](url, {'w' : 256})
+                    
+
+            # make html
+            gallery_html += "<div class='gallery-item gallery-%s'><a href='%s'><img src='%s' alt='%s' title='%s' /></a></div>" % (item['type'].lower(), url, image_src, title, title)
+
+
+        return gallery_html
---
+++ extension/tags.py
@@ -0,0 +1,72 @@
+"""
+    WebChomp -- A tool for generating static websites.
+    Copyright (C) 2014 Nathan Ogden
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+"""
+    Jinja extension, provides process of custom tags.
+"""
+
+import re, os, fnmatch, shutil, imp, itertools
+
+class jinja_extension:
+
+    def __init__(self, generator):
+        self.generator = generator
+
+        # load all tags
+        self.extensions = {}
+        for root, dirnames, filenames in itertools.chain( os.walk("extension/"), os.walk(self.generator.site_extension_path) ):
+            for filename in fnmatch.filter(filenames, '*.py'):
+                extension = imp.load_source(
+                    "tag_%s" % os.path.splitext(filename)[0],
+                    os.path.join(root, filename)
+                )
+                if hasattr(extension, 'tag_extension'):
+                    self.extensions["tag_%s" % os.path.splitext(filename)[0]] = extension.tag_extension(self.generator)
+
+        # load jinja functions
+        self.tags = {}
+        for extension in self.extensions:
+            # append functions
+            if hasattr(self.extensions[extension], "get_tags"):
+                self.tags = dict( self.tags.items() + self.extensions[extension].get_tags().items() )
+
+    def get_jinja_filters(self):
+        return {
+            'tags' : self.parse_tags
+        }
+
+    def get_tags(self):
+        return {
+            ''
+        }
+
+    # parses tags in given string
+    def parse_tags(self, string):
+
+        # regex to match
+        regex = re.compile("\[(.*?) (.*?)\]")
+
+        # match
+        for match in regex.findall(string):
+
+            # loop through all tags
+            for tag in self.tags:
+                if match[0].lower() == tag.lower():
+                    string = string.replace("[%s %s]" % (match[0], match[1]), self.tags[tag](match[1]))
+
+        return string
